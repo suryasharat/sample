ac8fdde6481b7e97d74fe4cb3a5a1a6396bb7c29
surya <surya@gmail.com>
2023-07-14 16:29:30 +0530

    two changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import Sample from './sample'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <Sample/>
     </div>
   );
 }
---
+++ src/sample.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sample() {
+  return (
+    <div>
+      <h1>Hi, this is surya</h1>
+    </div>
+  )
+}
+
+export default Sample;
